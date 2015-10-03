d79bd8a2bdca76779cfd2e94eada0325e7aad94f
Carl <carl@carlossus.com>
2015-10-03 19:23:16 +0800

    Add more documentation to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,20 @@
 export function generate(options: Options);
 
 export interface Options {
+    
+    /** Fixed width. Overrides min/max width */
     width?: number;
+    
     maxWidth?: number;
     
+    /** Fixed height. Overrides min/max height */
     height?: number;
+    
     maxHeight?: number;
     
+    /** Fixed depth. Overrides min/max depth */
     depth?: number;
+    
     maxDepth?: number;
 }
 
@@ -18,20 +25,25 @@ export interface Level {
 
 export interface Row {
     id: number;
+    
     /** Reference to parent Level */
     parent: Row;
+    
     cells: Array<Cell>;
 }
 
 export interface Cell {
     id: number;
+    
     /** Referece to parent Row */
     parent: Row;
 }
 
 export interface Coordinate {
+    
     /** Zero-based */
     row: number;
+    
     /** Zero-based */
     cell: number;
     
