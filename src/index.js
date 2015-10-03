157f29322373782b25b42c1779a04ff763004afb
Carl <carl@carlossus.com>
2015-10-03 19:20:43 +0800

    WIP: create entry point placeholder with algorithm description

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+/**
+ *   The algorithm
+
+    In this algorithm a "feature" is taken to mean any kind of map component e.g. large room, small room, corridor, circular arena, vault etc.
+    1. Fill the whole map with solid earth
+    2. Dig out a single room in the centre of the map
+    3. Pick a wall of any room
+    4. Decide upon a new feature to build
+    5. See if there is room to add the new feature through the chosen wall
+    6. If yes, continue. If no, go back to step 3
+    7. Add the feature through the chosen wall
+    8. Go back to step 3, until the dungeon is complete
+    9. Add the up and down staircases at random points in map
+    10. Finally, sprinkle some monsters and items liberally over dungeon
+ */
+function generate(options) {
+}
 //# sourceMappingURL=index.js.map
